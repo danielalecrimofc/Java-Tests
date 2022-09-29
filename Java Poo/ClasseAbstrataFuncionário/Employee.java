ac92f3a262d205cc4926e79b8174066dd8ca8b57
Daniel Alecrim <98345886+danielalecrimofc@users.noreply.github.com>
2022-09-28 21:39:52 -0300

    Class Employee

---
+++ Java Poo/ClasseAbstrataFuncionário/Employee.java
@@ -0,0 +1,25 @@
+public abstract class Employee {
+    protected String name;
+    protected double salary;
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public abstract void increaseSalary();
+}
